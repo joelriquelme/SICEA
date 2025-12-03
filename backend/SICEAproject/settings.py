3f724af01e15f4dbd4b2e610e8aaefc564e07b48
Joel Riquelme <joel.riquelme@ug.uchile.cl>
2025-12-02 23:57:19 -0300

    feat: add https://sicea.ing.uchile.cl to CORS

--- backend/SICEAproject/settings.py
+++ backend/SICEAproject/settings.py
@@ -150,6 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://sicea.ing.uchile.cl",
 ]
 
 CORS_ALLOW_CREDENTIALS = True 
